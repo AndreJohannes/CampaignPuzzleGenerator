5552c17e143d59a587afd35d00ce4a72928e1017
Andre <stoffel.andre@gmail.com>
2016-09-06 00:27:53 -0500

    work in progress

--- make_source.py
+++ make_source.py
@@ -7,8 +7,8 @@ class Source:
         ny = 30
         res_x = 768
         res_y = 1024
-        dx = 768 / nx
-        dy = 1024 / ny
+        dx = 768. / nx
+        dy = 1024. / ny
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 768 * 2, 1024 * 2)
         source = cairo.ImageSurface.create_from_png("image/bordado.png")
         for i in range(0, nx):
